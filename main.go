9b7c5dd13a4469e2e7f0b24c4f95583c1efa08c9
azimao <himaoyu@gmail.com>
2019-08-11 22:01:53 +0800

    first commit

--- main.go
+++ main.go
@@ -4,9 +4,21 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/topic/:topic_id", func(c *gin.Context) {
-		c.JSON(200, c.Param("topic_id"))
+	v1 := r.Group("/v1/topics")
+
+	v1.GET("", func(c *gin.Context) {
+		c.DefaultQuery("username", "azimao")
+		if c.Query("username") == "" {
+			c.String(200, "get something")
+		} else {
+			c.String(200, "get param:%s", c.Query("username"))
+		}
+		//c.JSON(200, c.Param("topic_id"))
 	})
+	v1.GET("/:topic_id", func(c *gin.Context) {
+		c.String(200, "%s", c.Param("topic_id"))
+	})
+
 	r.Run()
 	// 开启服务，默认端口是8080
 }
